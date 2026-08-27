1f50dd1e9b4d876e82ec7beaeb1811caf0df2af3
Boris Araujo <marinho.boris@gmail.com>
2026-08-27 14:33:30 -0300

    reajuste tipo de mapa

--- src/App.jsx
+++ src/App.jsx
@@ -58,9 +58,8 @@ function App() {
 
         <TileLayer
           onLoad={(e) => { e.target._map.invalidateSize() }}
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          subdomains="abc"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
         />
         <div id="map"></div>
       <LayerGroup>
